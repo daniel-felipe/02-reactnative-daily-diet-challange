51fcd4d86801d71e4e2260328a018f927057770f
= <daniell.felipe.2017@gmail.com>
2025-03-31 22:43:17 -0300

    configure app theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from 'react-native'
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/nunito-sans'
+import { ThemeProvider } from 'styled-components/native'
+
+import { Home } from '@/screens/Home'
+import theme from '@/theme'
 
 export default function App() {
+  useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  })
+
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   )
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,39 @@
+export default {
+  colors: {
+    red: {
+      dark: '#BF3B44',
+      base: '#F3BABD',
+      light: '#F4E6E7',
+    },
+
+    green: {
+      dark: '#639339',
+      base: '#CBE4B4',
+      light: '#E5F0DB',
+    },
+
+    gray: {
+      100: '#1B1D1E',
+      200: '#333638',
+      300: '#5C6265',
+      400: '#B9BBBC',
+      500: '#DDDEDF',
+      600: '#EFF0F0',
+      700: '#FAFAFA',
+    },
+
+    white: '#FFFFFF',
+  },
+  fontFamily: {
+    regular: 'NunitoSans_400Regular',
+    bold: 'NunitoSans_700Bold',
+  },
+  fontSize: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
+    '2xl': 32,
+  },
+}
